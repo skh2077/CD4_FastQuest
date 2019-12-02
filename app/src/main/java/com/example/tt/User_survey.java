9d97cf5c90280aa0cd03735a4d50bf54cc8e6129
ksebin <ohrmazd917@gmail.com>
2019-12-02 23:32:15 +0900

    설문 start ->바로 설문

--- app/src/main/java/com/example/tt/User_survey.java
+++ app/src/main/java/com/example/tt/User_survey.java
@@ -7,16 +7,30 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.SeekBar;
 import android.widget.Toast;
 
-public class User_survey extends AppCompatActivity {
-
+public class User_survey extends AppCompatActivity implements SeekBar.OnSeekBarChangeListener {
+    SeekBar seekBar1;
+    SeekBar seekBar2;
+    SeekBar seekBar3;
     ImageButton backButton;
     Button nextButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_usersurvey);
+
+
+        // initiate  views
+        seekBar1 = (SeekBar) findViewById(R.id.seekBar1);
+        seekBar2 = (SeekBar) findViewById(R.id.seekBar2);
+        seekBar3 = (SeekBar) findViewById(R.id.seekBar3);
+
+        seekBar1.setOnSeekBarChangeListener(this);
+        seekBar2.setOnSeekBarChangeListener(this);
+        seekBar3.setOnSeekBarChangeListener(this);
 
 
 
@@ -24,15 +38,63 @@ public class User_survey extends AppCompatActivity {
         backButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
             }
         });
         nextButton = findViewById(R.id.next);
         nextButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(getApplicationContext(),pre_cat.class));
+                startActivity(new Intent(getApplicationContext(), pre_cat.class));
             }
         });
     }
+
+
+    int value;
+
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+        value = progress;
+
+
+        switch (seekBar.getId()) {
+            case R.id.seekBar1:
+
+                break;
+
+            case R.id.seekBar2:
+
+                break;
+            case R.id.seekBar3:
+
+                break;
+        }
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+
+        switch (seekBar.getId()) {
+            case R.id.seekBar1:
+
+                break;
+
+            case R.id.seekBar2:
+
+                break;
+            case R.id.seekBar3:
+
+                break;
+        }
+        Toast.makeText(User_survey.this, "Seek bar progress is :" + value,
+                Toast.LENGTH_SHORT).show();
+    }
+
+
 }
